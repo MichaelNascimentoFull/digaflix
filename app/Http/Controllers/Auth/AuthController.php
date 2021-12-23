5dff1069de798d52aac3d483ccbaa923397f8aac
MichaelNascimentoFull <michaellopesn22@gmail.com>
2021-12-23 07:57:59 -0300

    Criação das apis de Login e Registro
    Criação do AuthController com login e registro em JWT, Rotas na api.php e VerificationController

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        if (!$token = auth()->attempt($validator->validated())) {
+            return response()->json(['error' => 'Senha e/ou email inválidos']);
+        }
+
+        return $this->createNewToken($token);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|between:2,100',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|confirmed|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::create(array_merge(
+            $validator->validated(),
+            ['password' => bcrypt($request->password)]
+        ));
+
+        return response()->json([
+            'message' => 'Usuário registrado com sucesso',
+            'user' => $user
+        ], 201);
+    }
+
+    protected function createNewToken($token)
+    {
+        $user = auth()->user();
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60,
+            'user' => $user
+        ]);
+    }
+}
