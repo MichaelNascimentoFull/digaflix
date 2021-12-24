de2934178d0ec258e1124587963d0ea356581a33
MichaelNascimentoFull <michaellopesn22@gmail.com>
2021-12-24 15:29:18 -0300

    Migrations e models de tags e tags_movies
    Migration criando tabela tags e tags_movies e também os moveis

---
+++ app/Models/Tag_Movie.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag_Movie extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['tag_id','movie_id'];
+
+    protected $table='tags_movies';
+}
+}
