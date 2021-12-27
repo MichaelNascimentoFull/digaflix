2518b326f8ccac6db350d2e97c214f0a681f2ad4
MichaelNascimentoFull <michaellopesn22@gmail.com>
2021-12-27 17:49:40 -0300

    Inicio da configuração dos testes automaticos

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_login_page()
+    {
+        $response = $this->get('/login');
+        
+        $response->assertStatus(200);
+    }
+    
+    public function test_login_url()
+    {
+        $response = $this->post('/api/auth/login',[
+            'email' => 'michaellopesn@gmail.com',
+            'password' => 'michaelln'
+        ]);
+        
+        $response->assertStatus(200);
+    }
+    
+}
